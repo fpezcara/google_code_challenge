268eb647ec3fa6db67e63f50fe4ccdb5991357e0
Rameez <rameez0khawaja@gmail.com>
2022-06-22 14:26:19 +0100

    new page

---
+++ client/resultsScript.js
@@ -0,0 +1,19 @@
+const searchButton = document.querySelector('#submit')
+
+searchButton.addEventListener('click', results)
+
+function results() {
+  let input = search.value
+  console.log(search.value)
+  fetch(`http://localhost:3000/home/${input}`)
+    .then(resp => resp.json())
+    .then(result => {
+        const mainList = document.querySelector("#resultsList")
+        mainList.innerHTML = ""
+        for (let i=0; i<result.length; i++){
+            const newList = document.createElement("li")
+            newList.textContent = result[i]
+            mainList.append(newList)
+        }
+    })
+}
