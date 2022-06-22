f526ad03ce6e37e5339f4bc761519c58c9589760
Florencia Pezcara <fpezcara@gmail.com>
2022-06-22 16:33:12 +0100

    finish polishing last details of the homepage's frontend

--- client/resultsScript.js
+++ client/resultsScript.js
@@ -1,23 +1,21 @@
-const searchButton = document.querySelector('#submit')
-searchButton.addEventListener('click', results)
+const searchButton = document.querySelector("#submit");
+searchButton.addEventListener("click", results);
 const input = require("./script.js");
 
-
-
-search.value = input
+search.value = input;
 
 function results() {
-  input = search.value
-  console.log(search.value)
+  input = search.value;
+  console.log(search.value);
   fetch(`http://localhost:3000/home/${input}`)
-    .then(resp => resp.json())
-    .then(result => {
-        const mainList = document.querySelector("#resultsList")
-        mainList.innerHTML = ""
-        for (let i=0; i<result.length; i++){
-            const newList = document.createElement("li")
-            newList.textContent = result[i]
-            mainList.append(newList)
-        }
-    })
+    .then((resp) => resp.json())
+    .then((result) => {
+      const mainList = document.querySelector("#resultsList");
+      mainList.innerHTML = "";
+      for (let i = 0; i < result.length; i++) {
+        const newList = document.createElement("li");
+        newList.textContent = result[i];
+        mainList.append(newList);
+      }
+    });
 }
